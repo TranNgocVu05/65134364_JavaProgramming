6645d376215277cb075395f05b0b45baaca0e0d4
Ngọc vũ <147695859+TranNgocVu05@users.noreply.github.com>
2026-01-21 12:23:07 +0700

    text hello word

---
+++ HelloWord/src/tnv/edu/Xinchao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tnv.edu;
+
+/**
+ * 
+ */
+public class Xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xin chao toi la tran ngoc vu");
+	}
+
+}
--- Hello_word_test.java
+++
@@ -1,6 +0,0 @@
-public class Hello_word_test
-{
-    public static void main(String[] args) {
-        System.out.println( "Xin chao toi la Tran Ngoc Vu");
-    }
-}
